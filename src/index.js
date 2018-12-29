32018cce2e807c5b519cdb7c6b470a208b5d0a78
Endre Vegh <endre.vegh@yahoo.com>
2018-12-29 21:20:52 +0100

    add manual rerender

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 import React, { render } from "./React";
 
-const App = <div>App</div>;
+const App = (
+  <div
+    style={{ backgroundColor: "red" }}
+    onClick={() => console.log("From the JSX!")}
+  >
+    App
+  </div>
+);
 
 const root = document.getElementById("root");
 
 render(App, root);
+
+const btn = document.createElement("button");
+
+btn.innerHTML = "Manually rerender!";
+
+const rerender = () =>
+  render(
+    <section
+      style={{ backgroundColor: "blue" }}
+      onClick={() => console.log("From new!")}
+    >
+      App in section
+    </section>,
+    root
+  );
+
+btn.addEventListener("click", rerender);
+
+root.appendChild(btn);
