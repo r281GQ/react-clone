2ca7124805e2a30dc2b083db9ac351718b29cf13
Endre Vegh <endre.vegh@yahoo.com>
2018-12-30 10:45:45 +0100

    multiple buttons in DOM

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const App = (
     style={{ backgroundColor: "red" }}
     onClick={() => console.log("From the JSX!")}
   >
-    App
+    <div style={{ textDecoration: "line-through" }}>Text1</div>
+    <div>Text2</div>
+    <div style={{ textDecoration: "line-through" }}>Text3</div>
+    <div>Text4</div>
   </div>
 );
 
@@ -13,21 +16,58 @@ const root = document.getElementById("root");
 
 render(App, root);
 
-const btn = document.createElement("button");
+const updateTypeMismatch = document.createElement("button");
+const updateSameType = document.createElement("button");
+const deletion = document.createElement("button");
 
-btn.innerHTML = "Manually rerender!";
+updateTypeMismatch.innerHTML = "Update with type mismatch!";
+updateSameType.innerHTML = "Update with same type!";
+deletion.innerHTML = "Delete something!";
 
-const rerender = () =>
+const updateTypeMismatchFunction = () =>
   render(
     <section
-      style={{ backgroundColor: "blue" }}
+      style={{ backgroundColor: "blue", color: "white" }}
       onClick={() => console.log("From new!")}
     >
-      App in section
+      <section>Text11</section>
+      <section style={{ textDecoration: "line-through" }}>Text22</section>
+      <section>Text33</section>
+      <section style={{ textDecoration: "line-through" }}>Text44</section>
     </section>,
     root
   );
 
-btn.addEventListener("click", rerender);
+const updateSameTypeFunction = () =>
+  render(
+    <div
+      style={{ backgroundColor: "blue", color: "white" }}
+      onClick={() => console.log("From new!")}
+    >
+      <div>Text11</div>
+      <div style={{ textDecoration: "line-through" }}>Text22</div>
+      <div>Text33</div>
+      <div style={{ textDecoration: "line-through" }}>Text44</div>
+    </div>,
+    root
+  );
 
-root.appendChild(btn);
+const deletionFunction = () =>
+  render(
+    <div
+      style={{ backgroundColor: "blue", color: "white" }}
+      onClick={() => console.log("From new!")}
+    >
+      <div>Text11</div>
+      <div style={{ textDecoration: "line-through" }}>Text44</div>
+    </div>,
+    root
+  );
+
+updateTypeMismatch.addEventListener("click", updateTypeMismatchFunction);
+updateSameType.addEventListener("click", updateSameTypeFunction);
+deletion.addEventListener("click", deletionFunction);
+
+root.appendChild(updateTypeMismatch);
+root.appendChild(updateSameType);
+root.appendChild(deletion);
