91544d0c44b9dff1dc29ddec2ade3ccfbb7a95d5
Endre Vegh <endre.vegh@yahoo.com>
2018-12-31 14:58:12 +0100

    add functional comp tag

--- src/React/Constants/FiberTags/index.js
+++ src/React/Constants/FiberTags/index.js
@@ -1,3 +1,4 @@
 export const CLASS_COMPONENT = "class_component";
+export const FUNCTIONAL_COMPONENT = "functional_component";
 export const HOST_COMPONENT = "host_component";
 export const HOST_ROOT = "host_root";
--- src/React/Misc/GetTag/index.js
+++ src/React/Misc/GetTag/index.js
@@ -1,8 +1,22 @@
-import { CLASS_COMPONENT, HOST_COMPONENT } from "./../../Constants";
+import {
+  CLASS_COMPONENT,
+  FUNCTIONAL_COMPONENT,
+  HOST_COMPONENT
+} from "./../../Constants";
+import { Component } from "./../../Component";
 
 /**
  *  getTag :: ReactElement -> String
  */
 export default element => {
-  return typeof element.type === "string" ? HOST_COMPONENT : CLASS_COMPONENT;
+  if (typeof element.type === "string") {
+    return HOST_COMPONENT;
+    /**
+     *  If it is a class based component we expect the prototype to be Component
+     */
+  } else if (Object.getPrototypeOf(element.type) !== Component) {
+    return FUNCTIONAL_COMPONENT;
+  } else {
+    return CLASS_COMPONENT;
+  }
 };
