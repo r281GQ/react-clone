318027fec7da34debd7ac16a7cb954a2a19399a8
Endre Vegh <endre.vegh@yahoo.com>
2019-01-01 13:24:52 +0100

    add hooks

---
+++ src/React/Hooks/index.js
@@ -0,0 +1,109 @@
+import {
+  getWorkInProgressFiber,
+  clearWorkInProgressFiber
+} from "./../Misc/WorkInProgressFiber";
+
+import { scheduleFunctionalUpdate } from "./../Reconciliation";
+
+/**
+ *  This is the wip Fiber at the time the hooks are called.
+ */
+let currentFiber;
+
+/**
+ *  This is the "state machine" implementation of the current hook being called.
+ *
+ *  The order of calling the hooks in the client matters because it has a fixed
+ *  order in the Fiber implementation of it.
+ */
+let currentHook;
+
+/**
+ *  getNextHook :: a -> Void
+ *
+ *  Sets the currentHook or creates one if its the initial render
+ *  to the appropriate one given the order how they were described in the client.
+ */
+const getNextHook = () => {
+  if (!currentHook) {
+    currentHook = currentFiber.memoizedState;
+  } else {
+    if (!currentHook.next) {
+      currentHook.next = {
+        memoizedState: undefined,
+        next: undefined,
+        queue: undefined
+      };
+    }
+
+    currentHook = currentHook.next;
+  }
+};
+
+/**
+ *  prepareHooks :: a -> Void
+ *
+ *  Sets the wip Fiber.
+ */
+const prepareHooks = () => {
+  currentFiber = getWorkInProgressFiber();
+};
+
+/**
+ *  cleanHooks :: a -> Void
+ *
+ *  Clears both the Fiber and hook global vars.
+ */
+const cleanHooks = () => {
+  currentFiber = null;
+
+  currentHook = null;
+
+  clearWorkInProgressFiber();
+};
+
+/**
+ *  rawSetter :: (Hook, Fiber) -> a -> Void
+ *
+ *  Creates a function that can be called outside of the context.
+ *
+ *  Pushed a new value onto the queue and triggers a rerender.
+ */
+const rawSetter = (currentHook, currentFiber) => value => {
+  currentHook.queue.push(value);
+
+  scheduleFunctionalUpdate(currentFiber);
+};
+
+/**
+ *  useState :: a -> [a, a -> Void]
+ */
+export const useState = initialState => {
+  getNextHook();
+
+  if (!currentHook.queue) {
+    currentHook.memoizedState = initialState;
+    currentHook.queue = [initialState];
+  } else {
+    currentHook.memoizedState = currentHook.queue[currentHook.queue.length - 1];
+  }
+
+  let value = currentHook.memoizedState;
+
+  let setter = rawSetter(currentHook, currentFiber);
+
+  return [value, setter];
+};
+
+/**
+ *  updateFunctionalComponent :: (a -> ReactElement) -> ReactElement
+ */
+export const updateFunctionalComponent = fn => {
+  prepareHooks();
+
+  const elements = fn();
+
+  cleanHooks();
+
+  return elements;
+};
--- src/React/index.js
+++ src/React/index.js
@@ -2,6 +2,7 @@ import createElement from "./createElement";
 
 export { Component } from "./Component";
 export { createRef } from "./CreateRef";
+export { useState } from "./Hooks";
 
 export default {
   createElement
