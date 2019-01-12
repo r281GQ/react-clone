d8b7cb1375739097293940e72b7d9dba9a8eb1d3
Endre Vegh <endre.vegh@yahoo.com>
2019-01-12 19:57:41 +0000

    add constants

--- src/React/Constants/FiberEffectTags/index.js
+++ src/React/Constants/FiberEffectTags/index.js
@@ -1,3 +1,5 @@
 export const PLACEMENT = "placement";
 export const UPDATE = "update";
 export const DELETION = "deletion";
+export const REPLACE_CREATE = "replace_create";
+export const REPLACE_DELETE = "replace_delete";
